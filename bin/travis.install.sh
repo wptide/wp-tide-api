b2b2063048b86c54aab3c30e8070c4610b551d43
Derek Herman <derek@valendesigns.com>
2018-06-09 00:03:37 -0700

    Try with pecl

--- bin/travis.install.sh
+++ bin/travis.install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cwd=$(pwd)
-
 # Get and make mongodb PHP driver
 sudo apt-get install libssl-dev \
     && git clone https://github.com/mongodb/mongo-php-driver.git --recursive \
@@ -14,17 +12,6 @@ sudo apt-get install libssl-dev \
     && cd .. \
     && rm -rf mongo-php-driver
 
-
-git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc
-cd grpc
-git pull --recurse-submodules && git submodule update --init --recursive
-make
-sudo make install
-
-cd src/php/ext/grpc
-phpize
-./configure
-make
-sudo make install
-
-cd $cwd
+// Install gRPC
+yes | sudo pecl install grpc
+echo "extension=grpc.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"` \

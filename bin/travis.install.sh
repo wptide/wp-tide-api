872cf17be85550b6c44994c6dc1de5d43e6dd41c
Derek Herman <derek@valendesigns.com>
2018-06-08 23:46:08 -0700

    Install gRPC

--- bin/travis.install.sh
+++ bin/travis.install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cwd=$(pwd)
+
 # Get and make mongodb PHP driver
 sudo apt-get install libssl-dev \
     && git clone https://github.com/mongodb/mongo-php-driver.git --recursive \
@@ -9,4 +11,20 @@ sudo apt-get install libssl-dev \
     && make all \
     && make install \
     && echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"` \
-    && cd ..
+    && cd .. \
+    && rm -rf mongo-php-driver
+
+
+git clone -b $(curl -L https://grpc.io/release) https://github.com/grpc/grpc
+cd grpc
+git pull --recurse-submodules && git submodule update --init --recursive
+make
+sudo make install
+
+cd src/php/ext/grpc
+phpize
+./configure
+make
+sudo make install
+
+cd $cwd

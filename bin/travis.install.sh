664da6ced1760d3f1667b387ae58495dd084a2ec
Derek Herman <derek@valendesigns.com>
2018-06-08 23:32:50 -0700

    Remove cache

--- bin/travis.install.sh
+++ bin/travis.install.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-set -e
-shopt -s expand_aliases
-
 # Get and make mongodb PHP driver
-if [[ ! -e $HOME/mongo-php-driver ]]; then
-    sudo apt-get install libssl-dev \
-        && git clone https://github.com/mongodb/mongo-php-driver.git --recursive \
-        && cd mongo-php-driver \
-        && phpize \
-        && ./configure --with-mongodb-ssl=openssl \
-        && make all \
-        && make install \
-        && echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"` \
-        && cd ..
-fi
+sudo apt-get install libssl-dev \
+    && git clone https://github.com/mongodb/mongo-php-driver.git --recursive \
+    && cd mongo-php-driver \
+    && phpize \
+    && ./configure --with-mongodb-ssl=openssl \
+    && make all \
+    && make install \
+    && echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"` \
+    && cd ..

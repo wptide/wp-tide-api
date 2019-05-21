d20761af643543cb77c8077a60265ed94cac54d5
Derek Herman <derek@valendesigns.com>
2019-05-21 04:25:02 -0700

    Revert file removal

---
+++ bin/travis.install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get and make mongodb PHP driver
+apt-get install libssl-dev \
+    && git clone https://github.com/mongodb/mongo-php-driver.git --recursive \
+    && cd mongo-php-driver \
+    && phpize \
+    && ./configure --with-mongodb-ssl=openssl \
+    && make all \
+    && make install \
+    && echo "extension=mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"` \
+    && cd .. \
+    && rm -rf mongo-php-driver
+
+// Install gRPC
+apt-get install php-pear \
+    && pecl channel-update pecl.php.net \
+    && pecl install grpc \
+    && echo "extension=grpc.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
